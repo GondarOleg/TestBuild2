f363af773d8263dc7b058230967dcfb8d38a7502
Oleg Gondar <Oleg_Gondar@epam.com>
2016-08-15 15:43:14 +0300

    1

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -2,6 +2,9 @@
  * Created by Oleg_Gondar on 8/15/2016.
  */
 public class Run {
+    public static void main(String[] args) {
+        
+    }
 
     public static boolean alwaysTrue(){
         return true;
